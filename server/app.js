29ac9bee42345b04697484ebb53a3fc5096749b6
Julian Festing <julfesting@gmail.com>
2024-02-28 15:10:33 +0000

    Add GET/ route and sperate app.listen into index.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,27 @@
-const express = require('express')
-require("dotenv").config()
+const express = require('express');
 
+const app = express();
 
-const app = express()
+// Handles requests that include a JSON payload in their body. e.g. POST
+app.use(express.json());
 
-const port = process.env.PORT
+app.get('/', (req, res) => {
+	res.send({
+		message: 'welcome',
+		description: 'Manga API',
+		endpoints: [
+			'GET    /            200',
+			'GET    /mangas       200',
+			'GET    /mangas/:id   200',
+			'POST   /mangas       201',
+			'PATCH  /mangas/:id   200',
+			'DELETE /mangas/:id   204',
+		],
+	});
+});
 
-app.listen(port, () => console.log(`Express departing now from ${port}`))
+app.post('/', (req, res) => {
+	res.status(405).send('Not allowd! Visit /mangas');
+});
+
+module.exports = app;
