340c1fab9135b31adb86a86d993fb8c379272b74
Julian Festing <julfesting@gmail.com>
2024-02-28 21:35:27 +0000

    create Manga class in models with getAll function

---
+++ server/models/Manga.js
@@ -0,0 +1,23 @@
+const db = require('../database/connection');
+
+class Manga {
+	constructor(data) {
+		this.id = data.id;
+		this.name = data.name;
+		this.date_published = data.date_published;
+		this.author = data.author;
+		this.description = data.description;
+	}
+
+	static async getAll() {
+		const response = await db.query(
+			"SELECT id, name, TO_CHAR(date_published, 'YYYY-MM-DD') AS date_published, author, description FROM manga"
+		);
+		if (response.rows.length === 0) {
+			throw new Error('No goats available.');
+		}
+		return response.rows.map(manga => new Manga(manga));
+	}
+}
+
+module.exports = Manga;
