e03e63fc5ffd9c96a49f5e4c9aa88dd8bf24a85d
Julian Festing <julfesting@gmail.com>
2024-02-28 16:03:10 +0000

    Add logger and cors to app

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
 
 const app = express();
 
 // Handles requests that include a JSON payload in their body. e.g. POST
 app.use(express.json());
+app.use(cors());
+app.use(logger('dev'));
 
 app.get('/', (req, res) => {
 	res.send({
