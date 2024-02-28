f2089120b852621f4f944b327e964d2a55266add
Julian Festing <julfesting@gmail.com>
2024-02-28 22:09:00 +0000

    remove unnecessary code from database connection

--- server/database/connection.js
+++ server/database/connection.js
@@ -6,20 +6,4 @@ const db = new Pool({
 	connectionString: process.env.DB_URL,
 });
 
-// This function is to test is a connection was made and does not contribute to functionality
-const checkConnection = async () => {
-	try {
-		const client = await db.connect();
-		// Check if client database matches DB_URL
-		if (client.database === 'ohsbdtkn') {
-			console.log('Connected to the database: ohsbdtkn');
-		}
-		client.release();
-	} catch (error) {
-		console.error('Error connecting to the database:', error);
-	}
-};
-
-checkConnection();
-
 module.exports = db;
